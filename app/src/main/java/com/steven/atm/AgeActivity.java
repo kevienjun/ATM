560e61ddf3e8fdcaf16d27063937a17510162c43
kevienjun <h1253516666@gmail.com>
2018-12-04 16:30:17 +0800

    ViewHolder

--- app/src/main/java/com/steven/atm/AgeActivity.java
+++ app/src/main/java/com/steven/atm/AgeActivity.java
@@ -3,18 +3,25 @@ package com.steven.atm;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.TextView;
 
 public class AgeActivity extends AppCompatActivity {
 
     private EditText edAge;
     private int age;
+    int [] ageChoose = {1,2,3,4,5,6,7,8,9};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_age);
+        RecyclerView ageRecycler = findViewById(R.id.recycler);
+        ageRecycler.setHasFixedSize(true);
+        ageRecycler.setLayoutManager(new LinearLayoutManager(this));
     }
     public void next(View view){
         edAge = findViewById(R.id.ed_age);
@@ -30,4 +37,15 @@ public class AgeActivity extends AppCompatActivity {
     public void back(View view){
         finish();
     }
-}
+
+    class ageAdapter {
+        class ageViewAdapter extends RecyclerView.ViewHolder{
+            TextView ageText;
+            public ageViewAdapter(View itemView) {
+                super(itemView);
+                ageText = itemView.findViewById(R.id.textv_age);
+            }
+        }
+    }
+        }
+
--- app/src/main/java/com/steven/atm/MainActivity.java
+++ app/src/main/java/com/steven/atm/MainActivity.java
@@ -5,8 +5,6 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
-
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
